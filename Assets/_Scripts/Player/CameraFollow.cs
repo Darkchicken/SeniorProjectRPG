c13cb91187a0c05a02570e939be2edd3dc23c8ff
jedisongoku <beratbekce@gmail.com>
2016-01-21 18:41:21 -0800

    Camera & Player Movement
    Camera is set to follow the player.
    Player is now have an AI movement with click to move.

---
+++ Assets/_Scripts/Player/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float smoothing = 5f;
+
+    private Transform player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - player.position;
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+
+}
---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float stopDistanceForAttack = 2f;
+
+    private Vector3 position;
+    private NavMeshAgent controller;
+    private Animator playerAnimation;
+    
+    private float idleTimer = 0f;
+    private bool isMoving = false;
+
+    void Awake()
+    {
+
+        controller = GetComponent<NavMeshAgent>();
+        playerAnimation = GetComponent<Animator>();
+        position = transform.position;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            locatePosition();
+
+        }
+
+        if (isMoving && controller.velocity == Vector3.zero)
+        {
+            idleTimer += Time.deltaTime;
+            if (idleTimer >= 0.04f)
+            {
+                isMoving = false;
+                playerAnimation.SetBool("IsMoving", false);
+                playerAnimation.SetTrigger("IDLE");
+                idleTimer = 0f;
+            }
+        }     
+    }
+
+    void locatePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            if (hit.transform.tag == "Enemy")
+            {
+                position = hit.transform.position;
+                controller.stoppingDistance = stopDistanceForAttack;
+            }
+            else
+            {
+                position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                controller.stoppingDistance = 0;
+            }
+        }
+        MoveToPosition();
+    }
+
+
+    void MoveToPosition()
+    {
+        transform.LookAt(position);
+        controller.SetDestination(position);
+
+        if (!isMoving)
+        {
+            playerAnimation.SetTrigger("RUN");
+            isMoving = true;
+            playerAnimation.SetBool("IsMoving", true);
+        }
+    }
+}
