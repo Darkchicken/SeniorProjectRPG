777a2dc11224c7cb873db6cf386ac6580c8d3e64
Kyugetsu <anguyenh91@gmail.com>
2016-01-27 23:08:55 -0800

    Warrior Stats
    I finished some preliminary stats and modifiers for the class. I
    couldn't really get a grasp on how to get components on other scripts so
    I need to add that later. I think I need to reference a gameobject?
    Everything is commented

---
+++ Assets/_Scripts/Player/Player Equipment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEquipment : MonoBehaviour 
+{
+    public int Str = 20;
+    public int Vit = 10;
+    public int Def = 30;
+	void Update () 
+    {
+	    //add gear stuff here later
+	}
+}
---
+++ Assets/_Scripts/Player/Warrior Stats.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WarriorStats : MonoBehaviour 
+{
+    
+    public int Level = 1;
+    public int Strength = 10;
+    public int Dexterity = 6;
+    public int Intelligence = 5;
+    public int Vitality = 10;
+    public int PlayerHealth;	//remove player health value on PlayerHealth Script; Set inheritence
+    public int Damage;
+    public int Defense;
+    public int TempTotalStr = 0;   // Get variable from PlayerEquipment
+    public int TempTotalVit = 0;    // Get variable from PlayerEquipment
+    public int TempTotalDef = 0;    // Get variable from PlayerEquipment
+    public float BaseExp = 0f;
+    public float ExpToLvl = 20f;
+
+    public int Currency = 0;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()               //constantly checks for any stat changes, exp changes, and modifies stats accordingly
+    {
+        CheckStr();
+        CheckVit();
+        DamageMod(Strength);
+        HealthMod(Vitality);
+        ExpCheck(BaseExp);
+    }
+
+    public void DamageMod(int Strength) //modifier to Damage
+    {
+        int tempStr = Strength % 6;
+        Damage = (tempStr * 6) + Strength; //Add weapon damage to formula
+        
+    }
+
+    public void HealthMod(int Vitality)     //modifier to HP
+    {
+        int tempVit = Vitality % 7;
+        PlayerHealth = (tempVit * 15) + (Vitality * 10);                       // total hp = BonusHP + playerHealth
+        
+    }
+
+    public void ExpCheck(float BaseExp)     //level up and exp checker
+    {
+        if (BaseExp == ExpToLvl)
+        {
+            Level++;
+            ExpToLvl = ExpToLvl * 10f;
+        }
+    }
+
+    public void CheckStr() //add total bonus strength from gear as an argument; adds bonus str from gear to str stat 
+    {
+        if (TempTotalStr < Strength || TempTotalStr > Strength)  //change Strength in condition to TotalGearStr
+        {
+            TempTotalStr = Strength - TempTotalStr;
+            Strength = Strength + TempTotalStr;
+        }
+    }
+
+    public void CheckVit()  //add total bonus vit from gear as an argument; adds bonus vit from gear to vit stat
+    {
+        if (TempTotalVit < Vitality || TempTotalVit > Vitality)  //change Vitality in condition to TotalGearVit
+        {
+            TempTotalVit = Vitality - TempTotalVit;
+            Vitality = Vitality + TempTotalVit;
+        }
+    }
+}
