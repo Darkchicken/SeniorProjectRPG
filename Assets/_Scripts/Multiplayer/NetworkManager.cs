95977cfd2b2715d0b40179faf344e289ba4be248
Darkchicken <joshshucker@hotmail.com>
2016-02-04 13:01:31 -0800

    Begin work on networking

---
+++ Assets/_Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        //version number of game is passed as variable
+        Debug.Log("PHOTON connection initialized");
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        Debug.Log("Arrives here");
+        
+       
+    }
+    void Update()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else
+        {
+            Debug.Log("AttemptingToConnect");
+            PhotonNetwork.CreateRoom(null);
+        }
+    }
+
+
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("Connected to Room");
+    }
+
+
+
+
+}
