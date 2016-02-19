8a8e6f2c367c069dd6b8a306378939f0af8de64d
Darkchicken <joshshucker@hotmail.com>
2016-02-18 21:46:12 -0800

    Added Photon calls back in
    Added photon code for random room
    Added button to find random room
    Removed old build code

--- Assets/_Scripts/Network/PhotonCalls.cs
+++ Assets/_Scripts/Network/PhotonCalls.cs
@@ -8,6 +8,7 @@ public class PhotonCalls : PunBehaviour
     //exits the current room
     public static void LeaveRoom()
     {
+        
         PhotonNetwork.LeaveRoom();
     }
     //when the player leaves their current room, reenter the lobby
@@ -18,6 +19,7 @@ public class PhotonCalls : PunBehaviour
     //upon reaching the lobby, join a random room 
     public override void OnJoinedLobby()
     {
+        Debug.Log("Looking for room to join");
         PhotonNetwork.JoinRandomRoom();
     }
 
@@ -25,10 +27,11 @@ public class PhotonCalls : PunBehaviour
     void OnPhotonRandomJoinFailed()
     {
         Debug.Log("Can't join random room!");
+        Debug.Log("Creating a new room!");
         ///argument is room name (null to assign a random name)
         PhotonNetwork.CreateRoom("Tester");
 
-        Debug.Log("Creating a new room!");
+       
     }
     //upon joining a new room, output the room name
     public override void OnJoinedRoom()
