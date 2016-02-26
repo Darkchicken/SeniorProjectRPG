96b8fee9c3cf372d2b92de40b77ad750f80eaefb
Darkchicken <joshshucker@hotmail.com>
2016-02-25 16:25:19 -0800

    Added transform scripting
    Player movement is now synced through NetworkPlayerScript in
    OnPhotonSerializeView function

--- Assets/_Scripts/Multiplayer/NetworkPlayerScript.cs
+++ Assets/_Scripts/Multiplayer/NetworkPlayerScript.cs
@@ -6,11 +6,19 @@ public class NetworkPlayerScript : MonoBehaviour {
 
     bool battleArena = true;
     PhotonView photonView;
+
+    Vector3 playerPos = Vector3.zero;
+    Quaternion playerRot = Quaternion.identity;
+    int playerHealth;
+
+
     // Use this for initialization
     void Start ()
     {
         //get the photon view of this character
         photonView = gameObject.GetComponent<PhotonView>();
+        //get player's health
+        playerHealth = GetComponent<Health>().health;
         //set proper name and tag to distinguish local player from others
         if (photonView.isMine)//isLocalPlayer)
         {
@@ -37,9 +45,38 @@ public class NetworkPlayerScript : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (photonView.isMine)
+        {
+            //do nothing, character is being controlled by player
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, playerPos, 0.1f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, playerRot, 0.1f);
+        }
+
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            //stream.SendNext(playerHealth);
+        }
+        else
+        {
+            //Network player, receive data
+            playerPos = (Vector3)stream.ReceiveNext();
+            playerRot = (Quaternion)stream.ReceiveNext();
+           // GetComponent<Health>().health = (int)stream.ReceiveNext();
+
+        }
+    }
 
 
 }
