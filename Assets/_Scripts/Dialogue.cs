e23684c6d9c1f99ffff6b0852888218bca5079bf
Darkchicken <joshshucker@hotmail.com>
2016-03-10 12:36:18 -0800

    Started writing code for Dialogue box
    Not implemented yet

---
+++ Assets/_Scripts/Dialogue.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+    GameObject dialogueBox;
+    Text dialogueText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dialogueBox = GameObject.Find("DialogueBox");
+        dialogueText = dialogueBox.transform.FindChild("DialogueText").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+    public void StartDialogue()
+    {
+        dialogueBox.SetActive(true);
+    }
+    public void DialogueMessage(string[] messages)
+    {
+        //get number of messages in dialogue
+        int messageCount = 0; 
+        int totalMessages = messages.Length;
+        //display first message
+        dialogueText.text = messages[0];
+        
+        while(messageCount != totalMessages)
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                //increase message count
+                messageCount++;
+                //display next message
+                dialogueText.text = messages[messageCount];
+            }
+        }
+        EndDialogue();
+    }
+    public void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
+}
