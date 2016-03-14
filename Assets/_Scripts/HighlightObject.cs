ed7bb7ba1912a274b8194a533d253bb0048ac0bb
Kyugetsu <anguyenh91@gmail.com>
2016-03-14 01:33:51 -0700

    Projector

---
+++ Assets/_Scripts/HighlightObject.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightObject : MonoBehaviour 
+{
+    RaycastHit hit;
+    public static GameObject CurrentSelectedObject;
+    public GameObject Target;
+    private static Vector3 mouseClickPoint;
+	
+	void Awake()
+    {
+        mouseClickPoint = Vector3.zero;
+    }
+	
+	
+	void Update () 
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                mouseClickPoint = hit.point;
+            }
+
+            if(hit.collider.name == "Terrain")
+            {
+                if(Input.GetMouseButtonDown(1))                         //for map projection later
+                {
+                    GameObject TargetObj = Instantiate(Target, hit.point, Quaternion.identity) as GameObject;
+                    TargetObj.name = "Target Instantiated";
+                }
+
+                else if(Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseClickPoint))
+                {
+                    DeselectGameObjectIfSelected();
+                }
+            }
+
+            else
+            {
+                if(Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseClickPoint))
+                {
+
+                    if(hit.collider.transform.FindChild("Selected"))
+                    {
+                        Debug.Log("Found a unit!");
+
+                        if(CurrentSelectedObject != hit.collider.gameObject)
+                        {
+                            GameObject SelectedObj = hit.collider.transform.FindChild("Selected").gameObject;
+                            SelectedObj.SetActive(true);
+
+                            if(CurrentSelectedObject != null)
+                            {
+                                CurrentSelectedObject.transform.FindChild("Selected").gameObject.SetActive(false);
+                            }
+
+                            CurrentSelectedObject = hit.collider.gameObject;
+
+                        }
+                        
+                    }
+                }
+            }
+
+        }
+        Debug.DrawRay(ray.origin, ray.direction * Mathf.Infinity, Color.yellow);
+    }
+
+#region Helper functions
+    public bool DidUserClickLeftMouse(Vector3 hitPoint) //mouse click checker
+    {
+        float clickZone = 0.8f;
+       
+        if((mouseClickPoint.x < hitPoint.x + clickZone && mouseClickPoint.x > hitPoint.x - clickZone) &&
+           (mouseClickPoint.x < hitPoint.x + clickZone && mouseClickPoint.x > hitPoint.x - clickZone) &&
+            (mouseClickPoint.x < hitPoint.x + clickZone && mouseClickPoint.x > hitPoint.x - clickZone))
+        {
+            return true;
+        }
+       
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void DeselectGameObjectIfSelected()       //deselects object when clicking on another thing
+    {
+        if(CurrentSelectedObject != null)
+        {
+            CurrentSelectedObject.transform.FindChild("Selected").gameObject.SetActive(false);
+            CurrentSelectedObject = null;
+        }
+    }
+
+#endregion
+}
---
+++ Assets/_Scripts/MousePoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePoint : MonoBehaviour 
+{
+    public GameObject Target;
+    RaycastHit hit;
+    private float raycastLength = 500;
+
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, raycastLength))
+        {
+    
+            if (hit.collider.name == "Terrain")
+            {
+
+                if(Input.GetMouseButtonDown(1)) //right click to instantiate target
+                {
+                    GameObject TargetObj = Instantiate(Target, hit.point, Quaternion.identity) as GameObject;
+                    TargetObj.name = "Target Instantiated";
+                }
+            }
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * raycastLength, Color.green);
+    }
+}
