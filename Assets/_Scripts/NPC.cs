af43c49cb7e8344db1aecb46a9c57737a2a020e8
Darkchicken <joshshucker@hotmail.com>
2016-03-15 13:32:46 -0700

    Added code for quest completion

--- Assets/_Scripts/NPC.cs
+++ Assets/_Scripts/NPC.cs
@@ -9,8 +9,17 @@ public class NPC : MonoBehaviour {
     Dialogue dialogueManager;
     [Header("Drop Quest Panel Here")]
     public GameObject questPanel;
-    [Header("Type Quest IDs for this NPC")]
+
+    //what quests this npc can grant
+    [Header("Quest IDs this NPC can start")]
     public string[] questId;
+    //what quests this npc can complete
+    [Header("Quest IDs this NPC can end")]
+    public string[] endQuestId;
+    
+
+    bool finishingQuest = false;
+
     GameObject player;
     Camera dialogueCamera;
     //offset from transform.position for each npc
@@ -22,6 +31,7 @@ public class NPC : MonoBehaviour {
     
     void Start()
     {
+       
         player = GameObject.FindGameObjectWithTag("Player");
         dialogueManager = GameObject.Find("GameManager").GetComponent<Dialogue>();
         dialogueCamera = GameObject.Find("DialogueCamera").GetComponent<Camera>();
@@ -29,6 +39,7 @@ public class NPC : MonoBehaviour {
     
     public void ClickedNPC()
     {
+        
         //activate dialogue box
         dialogueManager.StartDialogue(npcName,dialogue);
         dialogueCamera.transform.position = transform.position+faceLocation;
@@ -43,15 +54,46 @@ public class NPC : MonoBehaviour {
 
     public void OnMouseDown()
     {
+        /////////////////////////////test code
+        //PlayFabDataStore.playerCompletedQuests.Clear();
+        Debug.Log("Current Quests: ");
+        foreach(string quests in PlayFabDataStore.playerQuestLog)
+        {
+            Debug.Log(quests);
+        }
+        Debug.Log("Completed Quests: " );
+        foreach (string quests in PlayFabDataStore.playerCompletedQuests)
+        {
+            Debug.Log(quests);
+        }
+        ///////////////////////////
         //if the player is within 3 units of the npc
         if (Vector3.Distance(player.transform.position, transform.position) < 3)
         {
+            finishingQuest = false;
             ClickedNPC();
-            StartQuest(0);
+            for(int i = 0; i < endQuestId.Length; i++)
+            {
+                EndQuest(i);
+                if(finishingQuest == true)
+                {
+                    break;
+                }
+            }
+            //if you arent finishing a quest and this npc has quests to give
+            if (finishingQuest == false && questId.Length!=0)
+            {
+                StartQuest(0);
+            }
         }
     }
     public void StartQuest(int questIndex)
     {
+        //if this npc is not a quest giver
+        if(questId.Length == 0)
+        {
+            return;
+        }
         //if the player has not already accepted this quest or completed this quest
         if (!PlayFabDataStore.playerQuestLog.Contains(questId[questIndex]) 
             && !PlayFabDataStore.playerCompletedQuests.Contains(questId[questIndex]))
@@ -61,6 +103,27 @@ public class NPC : MonoBehaviour {
             questPanel.SetActive(true);
         }
     }
+    public void EndQuest(int questIndex)
+    {
+        //if this npc is not a quest ender
+        if (endQuestId.Length == 0)
+        {
+            return;
+        }
+        //if the player has accepted this quest and has not completed it
+        if (PlayFabDataStore.playerQuestLog.Contains(endQuestId[questIndex])
+            && !PlayFabDataStore.playerCompletedQuests.Contains(endQuestId[questIndex]))
+        {
+            //complete quest
+            finishingQuest = true;
+            PlayFabDataStore.playerCompletedQuests.Add(endQuestId[questIndex]);
+            PlayFabDataStore.playerQuestLog.Remove(endQuestId[questIndex]);
+            Debug.Log("You completed "+ endQuestId[questIndex]);
+            //questPanel.GetComponent<LoadQuest>().questId = questId[questIndex];
+            //questPanel.SetActive(true);
+        }
+    }
+
 
     void OnMouseOver()
     {
